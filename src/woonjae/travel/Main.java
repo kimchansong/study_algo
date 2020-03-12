73606eb06ab9d864d139dcd6db9e2bf7589250e3
woonjae.shin <mr0killer@gmail.com>
2020-03-12 10:48:47 +0900

    추가

--- src/woonjae/travel/Main.java
+++ src/woonjae/travel/Main.java
@@ -1,7 +1,6 @@
 package woonjae.travel;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -9,7 +8,11 @@ public class Main {
 	static int M;
 	static int K;
 
+	static int answer = Integer.MIN_VALUE;
+
 	static int[][] map;
+
+	static int[][] cache;
 
 	public static void main(String[] args) {
 		try (Scanner sc = new Scanner(System.in)) {
@@ -18,41 +21,65 @@ public class Main {
 			K = sc.nextInt(); // K 개의 항로가 있움
 
 			map = new int[N + 1][N + 1];
+			cache = new int[N + 1][N + 1];
 			// ArrayList<Line> list = new ArrayList<>();
 
 			// 최대치 이외에는 필요없을듯
 			for (int i = 0; i < K; i++) {
-				map[sc.nextInt()][sc.nextInt()] = sc.nextInt();
+				int x = sc.nextInt();
+				int y = sc.nextInt();
+				int value = sc.nextInt();
+
+				map[x][y] = Math.max(value, map[x][y]);
 			}
 
-			Queue<Node> q = new LinkedList<>();
-			q.add(new Node(1, 0));
-			while (!q.isEmpty()) {
-				Node v = q.poll();
-				for (int i = 0; i <= N; i++) {
+			// dfs(1, 1, 0);
+			// System.out.println();
 
+			for (int i = 0; i < cache.length; i++) {
+				Arrays.fill(cache[i], -1);
+			}
+
+			int value = dfs(1, 1);
+			System.out.println(value);
+			// print();
+
+		}
+
+	}
+
+	private static void print() {
+		for (int i = 0; i < cache.length; i++) {
+			System.out.print(cache[i] + " ");
+		}
+		System.out.println();
+	}
+
+	private static int dfs(int city, int count) {
+
+		// 도달 못했으
+		if (count >= M && city != N) {
+			return Integer.MIN_VALUE;
+		}
+
+		// N에 도달하믄 뭐 다음으로 갈곳이 없으니
+		// 기내식은 0
+		if (city == N) {
+			return 0;
+		}
+
+		// 온적없으면
+		if (cache[city][count] == -1) {
+			cache[city][count] = 0;
+
+			for (int i = city + 1 /* 큰 쪽으로만 가면되니까 */ ; i <= N; i++) {
+				if (map[city][i] > 0) {
+					// 현재 에서 다음으로 가는 기내식이랑
+					// 다음 값이랑
+					cache[city][count] = Math.max(cache[city][count], dfs(i, count + 1) + map[city][i]);
 				}
-
 			}
 		}
-
+		return cache[city][count];
 	}
-
-	static class Node {
-
-		Node() {
-		}
-
-		Node(int x, int value, int count) {
-			this.x = x;
-			this.value = value;
-			this.count = count;
-		}
-
-		int x = 0;
-		int value = 0;
-		int count;
-
-	}
-
 }
